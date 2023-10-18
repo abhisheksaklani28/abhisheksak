ebf60b42376ccb7196564d4f31df758ca4636271
Meenu <Meenu@LAPTOP-TOE98MBN>
2023-10-18 17:08:48 +0530

    commit 2nd time

---
+++ repository/src/local/raman.java
@@ -0,0 +1,16 @@
+package local;
+
+public class raman 
+{
+	public void m2()
+	{
+		System.out.println("m2 method ");
+		
+	}
+public static void main(String[] args) 
+{
+	raman obj=new raman();
+	obj.m2();
+	
+}
+}
