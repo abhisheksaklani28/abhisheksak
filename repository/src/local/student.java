681ff43b79af35a6bc7e6f17aec74f10452b66c2
Meenu <Meenu@LAPTOP-TOE98MBN>
2023-10-18 16:56:10 +0530

    pushing code 1stb time

---
+++ repository/src/local/student.java
@@ -0,0 +1,20 @@
+package local;
+
+public class student 
+{
+	public void m1()
+	{
+		System.out.println("this is method 1");
+	}
+	public void m2()
+	{
+		System.out.println("this is method 2");
+	}
+public static void main(String[] args) 
+{
+	student obj=new student();
+	obj.m1();
+	obj.m2();
+	
+}
+}
